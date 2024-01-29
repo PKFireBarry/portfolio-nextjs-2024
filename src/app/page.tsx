72dcce7c0256fd19e8c0b637496ead7900e0f238
Darion George <Barry0719@gmail.com>
2024-01-29 03:30:32 -0500

    Fix: Removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Nav from "@/components/Nav";
-import ParticleBackGround from "@/components/ParticleBackground";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Timeline from "@/components/Timeline";
